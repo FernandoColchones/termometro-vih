4438f1833d5b94bae6b682710f5fef777b2594fb
FernandoColchones <130452951+FernandoColchones@users.noreply.github.com>
2023-06-06 12:17:08 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    humedad = pins.analogReadPin(AnalogPin.P1)
+    radio.sendNumber(pins.analogReadPin(AnalogPin.P2))
+    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
+})
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showNumber(input.temperature())
     radio.sendNumber(input.temperature())
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
+    basic.pause(1000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
@@ -16,4 +29,14 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(1.8 * (input.temperature() + 32))
     radio.sendNumber(1.8 * (input.temperature() + 32))
 })
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    basic.pause(1000)
+    basic.clearScreen()
+})
+let humedad = 0
 radio.setGroup(677)
+basic.forever(function () {
+    serial.writeNumber(input.temperature())
+    serial.writeNumber(pins.analogReadPin(AnalogPin.P1))
+})
